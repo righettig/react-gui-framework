fe2639095074cdb1100bef182ee02b49d8305c6e
Giacomo Righetti <kirkrulez83@gmail.com>
2020-02-22 16:09:50 +0000

    Added react-grid-layout

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 import 'ag-grid-enterprise';
 
+import 'react-grid-layout/css/styles.css'
+import 'react-resizable/css/styles.css';
+import BasicLayout from './Layout/Layout';
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       columnDefs: [
-        { headerName: "Make", field: "make" }, 
-        { headerName: "Model", field: "model" }, 
+        { headerName: "Make", field: "make" },
+        { headerName: "Model", field: "model" },
         { headerName: "Price", field: "price" }
       ],
       rowData: [
-        { make: "Toyota", model: "Celica", price: 35000 }, 
-        { make: "Ford", model: "Mondeo", price: 32000 }, 
+        { make: "Toyota", model: "Celica", price: 35000 },
+        { make: "Ford", model: "Mondeo", price: 32000 },
         { make: "Porsche", model: "Boxter", price: 72000 }
       ]
     }
@@ -24,17 +28,20 @@ class App extends Component {
 
   render() {
     return (
-      <div
-        className="ag-theme-balham"
-        style={{
-          height: '500px',
-          width: '600px'
-        }}
-      >
-        <AgGridReact
-          columnDefs={this.state.columnDefs}
-          rowData={this.state.rowData}>
-        </AgGridReact>
+      <div>
+        <BasicLayout></BasicLayout>
+        <div
+          className="ag-theme-balham"
+          style={{
+            height: '500px',
+            width: '600px'
+          }}
+        >
+          <AgGridReact
+            columnDefs={this.state.columnDefs}
+            rowData={this.state.rowData}>
+          </AgGridReact>
+        </div>
       </div>
     );
   }
