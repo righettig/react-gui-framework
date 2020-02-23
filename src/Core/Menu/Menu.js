eb0dcaca6b01cfb4314759cf894484876795965b
Giacomo Righetti <kirkrulez83@gmail.com>
2020-02-23 14:20:23 +0000

    Added routing

--- src/Core/Menu/Menu.js
+++ src/Core/Menu/Menu.js
@@ -3,15 +3,44 @@ import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+import { BrowserRouter as Router, Switch, Route, Link, NavLink } from 'react-router-dom';
+
 export default function Menu(props) {
-  return <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">INTL</Navbar.Brand>
-    <Nav className="mr-auto">
-      {
-        props.modules.map((module) =>
-          <Nav.Link href="#home">{module.displayName}</Nav.Link>
-        )
-      }
-    </Nav>
-  </Navbar>
+  return (
+    <Router>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand>INTL</Navbar.Brand>
+        <Nav className="mr-auto">
+          {
+            props.modules.map((module) =>
+              <NavLink to={'/' + module.displayName} className="nav-link">{module.displayName}</NavLink>
+            )
+          }
+        </Nav>
+      </Navbar>
+      <Switch>
+        <Route path="/orders">
+          <Orders />
+        </Route>
+        <Route path="/trades">
+          <Trades />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Trades() {
+  return <h2>Trades</h2>;
+}
+
+function Orders() {
+  return <h2>Orders</h2>;
 }
